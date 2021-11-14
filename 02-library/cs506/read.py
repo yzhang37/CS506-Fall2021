5a101f1f85f7d334ce7dfde7021263dcf14c96f6
青椒 <johnyin@bu.edu>
2021-11-14 02:44:33 -0500

    library: read_csv completed.

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,15 @@
+import csv
+
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    data = []
+    with open(csv_file_path) as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for row in csv_reader:
+            row_data = [float(x) for x in row]
+            data.append(row_data)
+    return data
